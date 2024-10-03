dd3058f95641b9628b149806b1a16e3977dd2bd1
pierremrtn <pierre2tm@gmail.com>
2024-10-03 14:54:05 +0700

    introduce firestore_modeler package

---
+++ firestore_modeler/README.md
@@ -0,0 +1,3 @@
+Runtime dependency for clients generated with [FirestoreModeler](https://firestoremodeler.com).
+
+At the moment, this package only re-export cloud_firestore_odm.
---
+++ firestore_modeler/lib/firestore_modeler.dart
@@ -0,0 +1,6 @@
+/// Runtime dependency for clients generated with [FirestoreModeler](https://firestoremodeler.com).
+///
+/// At the moment, this package only re-export cloud_firestore_odm.
+library;
+
+export 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
